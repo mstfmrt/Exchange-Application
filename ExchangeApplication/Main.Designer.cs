3847a974409ec8091cc8a30e0a08a85b25e65edb
Mustafa MERT <69737258+mstfmrt@users.noreply.github.com>
2022-12-03 01:17:54 +0300

    Initial commit

---
+++ ExchangeApplication/Main.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ExchangeApplication
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.submit = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.export = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(33, 19);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2022, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(1997, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(155, 27);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(222, 19);
+            this.submit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(86, 27);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Göster";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(591, 749);
+            this.webView21.TabIndex = 2;
+            this.webView21.ZoomFactor = 0.5D;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 749);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 65);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webView21);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1182, 749);
+            this.splitContainer1.SplitterDistance = 591;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // export
+            // 
+            this.export.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.export.Location = new System.Drawing.Point(980, 19);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(173, 27);
+            this.export.TabIndex = 5;
+            this.export.Text = "Excel Dışa Aktar";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 814);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Main";
+            this.Text = "Döviz Kuru";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Button submit;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private DataGridView dataGridView1;
+        private SplitContainer splitContainer1;
+        private Button export;
+    }
+}
---
+++ ExchangeApplication/Main.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Globalization;
+using ClosedXML.Excel;
+using System.Reflection.Metadata;
+using System.Windows.Forms;
+
+namespace ExchangeApplication
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            webView21.Source = new Uri("https://www.tcmb.gov.tr/kurlar/kurlar_tr.html");
+
+            dataGridView1.ColumnCount = 7;
+            dataGridView1.Columns[0].Name = "Doviz Kodu"; // dataGridViev sütun açıklamaları tanımlanması
+            dataGridView1.Columns[1].Name = "Birim";
+            dataGridView1.Columns[2].Name = "Doviz Cinsi";
+            dataGridView1.Columns[3].Name = "Doviz Alis";
+            dataGridView1.Columns[4].Name = "Doviz Satis";
+            dataGridView1.Columns[5].Name = "Elektif Alis";
+            dataGridView1.Columns[6].Name = "Elektif Satis";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            export.Enabled = false;     //excel dışa aktarma butonu herhangi bir data yüklenmediğinde basılamaz durumda olmalı
+
+        }
+
+        private async void submit_Click(object sender, EventArgs e)
+        {
+            submit.Enabled = false; //bir data yüklenirken ikinci bir istekte bulunulmaması amacıyla süreç bitene kadar buton basılamaz durumda olacaktır
+            string buildedRequestXmlUrl = dateTimePicker1.Value.ToString("yyyyMM") + '/' + dateTimePicker1.Value.ToString("ddMMyyyy") + ".xml";
+            string buildedRequestXslUrl = dateTimePicker1.Value.ToString("yyyyMM") + '/' + "isokur-new" + ".xsl";
+            _ = webView21.CoreWebView2.ExecuteScriptAsync(
+                "displayResult(\"" + buildedRequestXslUrl + "\",\"" + buildedRequestXmlUrl + "\"); " +
+                "$(\"#back\").show();" +
+                "$(\"#data\").show();" +
+                "$(\"#calendar_report\").hide();" +
+                "$(\".footnote\").hide();");
+            Thread.Sleep(500);
+            var rowAmount = await webView21.CoreWebView2.ExecuteScriptAsync(
+                "var o = document.getElementsByClassName(\"kurlarTablo\");" +
+                "var rows = o[0].lastChild.childNodes;" +
+                "rows.length"
+                );
+            dataGridView1.Rows.Clear();
+            export.Enabled= false;
+            bool colorControl = true; //dataGridView renk formatlamasında önceki satırın rengine göre renk değişikliği yapmak için bool değer
+            if (rowAmount != "null")    //sayfaya tablo yüklenmiş ise data işlenerek dataGrıdView e aktarılır
+            {   
+                for (int i = 0; i < int.Parse(rowAmount); i += 2)
+                {
+                    string kurKodu = await webView21.CoreWebView2.ExecuteScriptAsync("var con = rows[" + i + "].getElementsByTagName(\"td\")[0].textContent; con");
+                    kurKodu = kurKodu.Replace(" ", "").Replace("\\", "").Replace("r", "").Replace("n", "").Replace("t", ""); // webSitesi üzerinden gelen kur kodu strınginin gereksiz karakterlerden arındırılması
+                    string birim = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[1].textContent");
+                    string dovizCinsi = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[2].textContent");
+                    string dovizAlis = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[3].textContent");
+                    string dovizSatis = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[4].textContent");
+                    string elektifAlis = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[5].textContent");
+                    string elektifSatis = await webView21.CoreWebView2.ExecuteScriptAsync("rows[" + i + "].getElementsByTagName(\"td\")[6].textContent");
+                    dataGridView1.Rows.Add(new[] {  kurKodu.Substring(1,kurKodu.Length - 2),
+                                                birim.Substring(1,birim.Length - 2),
+                                                dovizCinsi.Substring(1,dovizCinsi.Length - 2),
+                                                dovizAlis.Substring(1,dovizAlis.Length - 2),
+                                                dovizSatis.Substring(1,dovizSatis.Length - 2),
+                                                elektifAlis.Substring(1,elektifAlis.Length - 2),
+                                                elektifSatis.Substring(1,elektifSatis.Length - 2)});
+                    if (colorControl)
+                    {
+                        dataGridView1.Rows[i / 2].DefaultCellStyle.BackColor = Color.LightGray;
+                        colorControl = false;
+                    }
+                    else colorControl = true;
+                }
+                export.Enabled = true; // data yüklendikten sonra excel dışa aktar butonu aktif edilir
+            }
+            else // sayfaya tablo yüklenmemişse TCBM sisteminin de verdiği hata mesajı messageBox olarak gösterilir
+            {
+                MessageBox.Show("Resmi tatil, hafta sonu ve yarım iş günü çalışılan günlerde gösterge niteliginde kur bilgisi yayımlanmamaktadır.");
+            }
+            submit.Enabled = true; // data yükleme işlemi bittiği için yeni data gösterme talebi için buton aktif edilmiştir 
+        }
+
+        public bool exportExcel(string fileName)
+        {
+            bool isExported = true;
+            using(IXLWorkbook workbook = new XLWorkbook())
+            {
+                workbook.AddWorksheet("Sheet 1").FirstCell().InsertData(fileName);
+                bool colorControl = true; // excel dosyasının renk formatlaması için önceki satırın renk kontrolünü sağlayan bool değer
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+                    workbook.Worksheet("Sheet 1").Cell(1, i).SetValue<string>(dataGridView1.Columns[i - 1].HeaderText); //sütun açıklamalarının aktarılması
+                }
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)//dataGridView içerisindeki data excel dosyasına kopyalanır
+                    {
+                        workbook.Worksheet("Sheet 1").Cell(i+2, j+1).SetValue<string>(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                        if (colorControl)
+                            workbook.Worksheet("Sheet 1").Cell(i + 2, j + 1).Style.Fill.BackgroundColor = XLColor.LightGray;
+                    }
+                    if(colorControl)
+                        colorControl= false;
+                    else colorControl= true;
+                }
+                workbook.Worksheet("Sheet 1").Columns().AdjustToContents(); //sütun genişlikleri veriye göre ayarlanır
+                workbook.Worksheet("Sheet 1").Range(1, 1, dataGridView1.RowCount, dataGridView1.Columns.Count).Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+                workbook.Worksheet("Sheet 1").Range(1, 1, dataGridView1.RowCount, dataGridView1.Columns.Count).Style.Border.OutsideBorder = XLBorderStyleValues.Thick;
+                workbook.Worksheet("Sheet 1").Range(1, 1, 1, 7).Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+                workbook.SaveAs(fileName);
+                isExported = true;
+            }
+            return isExported;
+        }
+
+        private void export_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.OverwritePrompt = false;
+            save.CreatePrompt = true;
+            save.InitialDirectory = @"D:\";
+            save.Title = "Excel Dosyaları";
+            save.DefaultExt = "xlsx";
+            save.Filter = "xlsx Dosyaları (*.xlsx)|*.xlsx|Tüm Dosyalar(*.*)|*.*";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                if (exportExcel(save.FileName))
+                {
+                    MessageBox.Show("Dosya dışa aktarıldı.");
+                };
+            }
+            
+        }
+    }
+}
